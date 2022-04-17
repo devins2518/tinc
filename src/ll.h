0c69c2ceea48647d868a0dbe34c4a5dc1ba76c22
Devin Singh <drsingh2518@icloud.com>
2022-04-17 09:22:59 -0500

    fix ll impl

--- src/ll.h
+++ src/ll.h
@@ -2,41 +2,48 @@
 #define LL_H
 
 #define DECLARE_LINKED_LIST(t)                                                                     \
-    typedef struct ll_##t {                                                                        \
-        struct ll_##t *next;                                                                       \
-        struct ll_##t *prev;                                                                       \
+    typedef struct ll_##t##_node {                                                                 \
+        struct ll_##t##_node *next;                                                                \
+        struct ll_##t##_node *prev;                                                                \
         t data;                                                                                    \
+    } ll_##t##_node;                                                                               \
+    typedef struct {                                                                               \
+        ll_##t##_node *start;                                                                      \
+        ll_##t##_node *end;                                                                        \
     } ll_##t;                                                                                      \
     ll_##t *ll_##t##_new();                                                                        \
-    void ll_##t##_append(ll_##t *ll, t d);
+    void ll_##t##_add_end(ll_##t *ll, t d);                                                        \
+    void ll_##t##_add_start(ll_##t *ll, t d);
 
 #define IMPL_LINKED_LIST(t)                                                                        \
-    ll_##t *ll_##t##_new(t data) {                                                                 \
-        ll_##t *ll = malloc(sizeof(ll_##t));                                                       \
-        if (ll == NULL) {                                                                          \
-            printf("Allocation failed.");                                                          \
+    ll_##t##_node *ll_##t##_node_new(t data) {                                                     \
+        ll_##t##_node *node = malloc(sizeof(ll_##t##_node));                                       \
+        if (node == NULL) {                                                                        \
+            printf("Allocation failed");                                                           \
             exit(EXIT_FAILURE);                                                                    \
         }                                                                                          \
-        ll->next = NULL;                                                                           \
-        ll->prev = NULL;                                                                           \
-        ll->data = data;                                                                           \
+        node->data = data;                                                                         \
+        node->next = NULL;                                                                         \
+        node->prev = NULL;                                                                         \
+        return node;                                                                               \
+    }                                                                                              \
+    ll_##t *ll_##t##_new() {                                                                       \
+        ll_##t *ll = malloc(sizeof(ll_##t));                                                       \
+        if (ll == NULL) {                                                                          \
+            printf("Allocation failed");                                                           \
+            exit(EXIT_FAILURE);                                                                    \
+        }                                                                                          \
         return ll;                                                                                 \
     }                                                                                              \
     void ll_##t##_add_end(ll_##t *ll, t d) {                                                       \
-        ll_##t *ln = ll;                                                                           \
-        while (ln->next != NULL) {                                                                 \
-            ln = ln->next;                                                                         \
-        }                                                                                          \
-        ln->next = ll_##t##_new(d);                                                                \
-        ln->next->prev = ln;                                                                       \
+        ll_##t##_node *last = ll_##t##_node_new(d);                                                \
+        ll->end->prev = last;                                                                      \
+        ll->end = last;                                                                            \
     }                                                                                              \
     void ll_##t##_add_start(ll_##t *ll, t d) {                                                     \
-        ll_##t *lp = ll;                                                                           \
-        while (lp->prev != NULL) {                                                                 \
-            lp = lp->prev;                                                                         \
-        }                                                                                          \
-        lp->prev = ll_##t##_new(d);                                                                \
-        lp->prev->next = lp;                                                                       \
+        ll_##t##_node *start = ll_##t##_node_new(d);                                               \
+        ll->start->next = start;                                                                   \
+        ll->start = start;                                                                         \
     }
 
 #endif
